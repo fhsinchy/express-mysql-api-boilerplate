d2fb321b8b43c249c21532bf2b735f0db0ea3392
Farhan Hasin Chowdhury <mail@farhan.info>
2020-04-28 13:53:33 +0600

    :heavy_plus_sign: Added cookie-parser and json middlewares

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import logger from 'morgan';
+import cookieParser from 'cookie-parser';
 
 import routes from '../routes';
 
 const server = express();
 
 server.use(logger('dev'));
+server.use(express.json());
+server.use(cookieParser());
 
 server.use('/', routes);
 
