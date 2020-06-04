3f075870eecd1bf8f67ffc9647ea09553da4ec87
Farhan Hasin Chowdhury <mail@farhan.info>
2020-06-04 17:44:05 +0600

    Proper error handling implemented

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { Router } = require('express');
+const { Bounce } = require('../middleware/bouncer');
 
 const db = require('../db');
 const authMiddleware = require('../middleware/auth');
@@ -10,14 +11,11 @@ dotenv.config();
 
 const router = Router();
 
-router.post('/register', async (req, res) => {
-  if (await db('users').where({ email: req.body.email }).first()) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Email Already Taken!',
-    });
-  } else {
-    try {
+router.post('/register', async (req, res, next) => {
+  try {
+    if (await db('users').where({ email: req.body.email }).first()) {
+      throw new Bounce(400, 'Email Already Taken!');
+    } else {
       await db('users').insert({
         name: req.body.name,
         email: req.body.email,
@@ -30,117 +28,101 @@ router.post('/register', async (req, res) => {
         status: 'success',
         message: 'User Registered!',
       });
-    } catch (err) {
-      res.status(500).json({
-        status: 'error',
-        message: err.message,
-      });
     }
+  } catch (err) {
+    next(err);
   }
 });
 
-router.post('/login', async (req, res) => {
-  const user = await db('users').where({ email: req.body.email }).first();
+router.post('/login', async (req, res, next) => {
+  try {
+    const user = await db('users').where({ email: req.body.email }).first();
 
-  if (!user) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Wrong Email or Password!',
-    });
-  } else if (await bcrypt.compare(req.body.password, user.password)) {
-    const tokenPayload = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-    };
+    if (!user) {
+      throw new Bounce(400, 'Wrong Email!');
+    } else if (await bcrypt.compare(req.body.password, user.password)) {
+      const tokenPayload = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      };
 
-    const accessToken = jwt.sign(tokenPayload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.NODE_ENV === 'development' ? '1d' : '5m' });
-    let refreshToken = await db('tokens').where({ user_id: user.id }).first();
+      const accessToken = jwt.sign(tokenPayload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.NODE_ENV === 'development' ? '1d' : '5m' });
+      let refreshToken = await db('tokens').where({ user_id: user.id }).first();
 
-    if (!refreshToken) {
-      refreshToken = jwt.sign(tokenPayload, process.env.REFRESH_TOKEN_SECRET);
+      if (!refreshToken) {
+        refreshToken = jwt.sign(tokenPayload, process.env.REFRESH_TOKEN_SECRET);
 
-      try {
         await db('tokens').insert({
           token: refreshToken,
           user_id: user.id,
         });
-      } catch (err) {
-        res.status(500).json({
-          status: 'error',
-          message: err.message,
-        });
+      } else {
+        refreshToken = refreshToken.token;
       }
-    }
 
-    res.cookie('refreshToken', refreshToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: true,
-    });
-
-    res.status(200).json({
-      status: 'success',
-      message: 'User Logged In!',
-      accessToken,
-    });
-  }
-});
-
-router.post('/token/refresh', async (req, res) => {
-  const { refreshToken } = req.cookies;
-
-  if (!refreshToken) {
-    res.status(401).json({
-      status: 'fail',
-      message: 'Unauthorized!',
-    });
-  }
-
-  const token = await db('tokens').where({ token: refreshToken }).first();
-
-  if (!token) {
-    res.status(403).json({
-      status: 'fail',
-      message: 'Forbidden!',
-    });
-  } else {
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async (err, user) => {
-      if (err) {
-        res.status(403).json({
-          status: 'fail',
-          message: err.message,
-        });
-      }
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30m' });
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: true,
+      });
 
       res.status(200).json({
         status: 'success',
-        message: 'Token Generated!',
+        message: 'User Logged In!',
         accessToken,
       });
-    });
+    } else {
+      throw new Bounce(400, 'Wrong Password!');
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
-router.post('/logout', authMiddleware, async (req, res) => {
-  const { refreshToken } = req.cookies;
+router.post('/token/refresh', async (req, res, next) => {
+  try {
+    const { refreshToken } = req.cookies;
 
-  if (!refreshToken) {
-    res.status(401).json({
-      status: 'fail',
-      message: 'Unauthorized!',
-    });
-  } else {
-    try {
+    if (!refreshToken) {
+      throw new Bounce(401, 'Unauthorized!');
+    }
+
+    const token = await db('tokens').where({ token: refreshToken }).first();
+
+    if (!token) {
+      throw new Bounce(403, 'Forbidden!');
+    } else {
+      jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async (err, user) => {
+        if (err) {
+          throw new Bounce(403, err.message);
+        }
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30m' });
+
+        res.status(200).json({
+          status: 'success',
+          message: 'Token Generated!',
+          accessToken,
+        });
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/logout', authMiddleware, async (req, res, next) => {
+  try {
+    const { refreshToken } = req.cookies;
+
+    if (!refreshToken) {
+      throw new Bounce(401, 'Unauthorized!');
+    } else {
       const token = await db('tokens').where({ token: refreshToken }).first();
 
       if (!token) {
-        res.status(403).json({
-          status: 'fail',
-          message: 'Forbidden!',
-        });
+        throw new Bounce(403, 'Forbidden!');
       } else {
         await db('tokens').where({ token: refreshToken }).del();
 
@@ -151,12 +133,9 @@ router.post('/logout', authMiddleware, async (req, res) => {
           message: 'Logged Out!',
         });
       }
-    } catch (err) {
-      res.status(500).json({
-        status: 'error',
-        message: err.message,
-      });
     }
+  } catch (err) {
+    next(err);
   }
 });
 
