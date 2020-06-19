df9e4cf529fb9b87c1bf18f876f757044c1587f5
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2020-06-19 21:35:10 +0600

    Fixed db username issue

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      user: process.env.DB_USERNAME,
+      user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
     },
@@ -33,7 +33,7 @@ module.exports = {
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      user: process.env.DB_USERNAME,
+      user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
     },
